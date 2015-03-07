e6ab1e8958a3d052d4069aac4a4011a1fede91c0
bigtallbill <bigtallbill@gmail.com>
2015-03-06 23:19:32 -0500

    Add read-all command

---
+++ src/Bigtallbill/PasswordLocker/Commands/CommandReadAll.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bigtallbill
+ * Date: 2/22/15
+ * Time: 12:40 AM
+ */
+
+namespace Bigtallbill\PasswordLocker\Commands;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CommandReadAll extends CommandBase
+{
+    protected function configure()
+    {
+        $this->setName('read-all')->setDescription('read all passwords');
+        parent::configure();
+        $this->addOption(
+            'b64d',
+            null,
+            InputOption::VALUE_NONE,
+            'When supplied will decode the passwords to readable text'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->configureCryptMethod($input);
+        $this->getPassword($output);
+        $this->decryptFile($input, $output);
+
+        $decrypted = $this->passwordLocker->getDecrypted();
+
+        if ($input->getOption('b64d')) {
+            $decryptedB64d = array();
+            foreach ($decrypted as $key => $value) {
+                $decryptedB64d[base64_decode($key)] = base64_decode($value);
+            }
+            $decrypted = $decryptedB64d;
+        }
+
+        $table = new Table($output);
+        $table->setHeaders(array('Key', 'Password'));
+
+        foreach ($decrypted as $key => $pass) {
+            $table->addRow(array($key, $pass));
+        }
+
+        $table->render();
+    }
+}
